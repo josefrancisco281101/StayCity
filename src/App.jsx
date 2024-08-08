616265b1e123614e09a2cbdff7a861bd4fc44394
josefrancisco <josefrancisco281101@gmail.com>
2024-08-08 10:32:02 -0400

    logita de filtrado completado

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,26 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Card } from "./components/Card/Card";
 import { Footer } from "./components/Footer/Footer";
 export default function App() {
+  const [data, setData] = useState([]);
+  const [cards, setCards] = useState([]);
+  async function getData() {
+    try {
+      const response = await fetch("/stays.json");
+      const jsonResponse = await response.json();
+      setCards(jsonResponse);
+      setData(jsonResponse);
+    } catch (error) {
+      console.error("Error al cargar los datos:", error);
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
-      <Navbar />
-      <Card />
+      <Navbar data={data} setCards={setCards} />
+
+      <Card cards={cards} />
       <Footer />
     </>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,63 @@
+import React, { useEffect, useState } from "react";
 import "./navbar.css";
 
-export const Navbar = () => {
+export const Navbar = ({ data, setCards }) => {
+  const [citys, setCitys] = useState([]);
+  const [searchValues, setSearchValues] = useState({
+    city: "",
+    maxGuests: "",
+  });
+
+  function filterData() {
+    const { city, maxGuests } = searchValues;
+    if (city.trim() !== "" && maxGuests === "") {
+      const rs = data.filter((card) =>
+        card.city.toLowerCase().includes(city.toLowerCase())
+      );
+      setCards(rs);
+      return;
+    }
+    if (city.trim() === "" && maxGuests !== "") {
+      const rs = data.filter((card) => card.maxGuests >= parseInt(maxGuests));
+
+      setCards(rs);
+      return;
+    }
+    if (city.trim() !== "" && maxGuests !== "") {
+      const rs = data.filter(
+        (card) =>
+          card.city.toLowerCase().includes(city.toLowerCase()) &&
+          card.maxGuests >= parseInt(maxGuests)
+      );
+      setCards(rs);
+      return;
+    }
+    setCards(data);
+  }
+  const handleChange = (e) => {
+    const target = e.target;
+    setSearchValues({ ...searchValues, [target.name]: target.value });
+  };
+  const getCitys = () => {
+    const rs = [];
+    data.forEach((city) => {
+      if (!rs.includes(city.city)) {
+        rs.push(city.city);
+      }
+    });
+    return rs;
+  };
+  useEffect(() => {
+    const rs = getCitys();
+    setCitys(rs);
+  }, []);
   const navOption = [
-    {
-      id: 1,
-      city: "Helsinki",
-      country: "Finland",
-    },
-    {
-      id: 2,
-      city: "Turku",
-      country: "Finland",
-    },
-    {
-      id: 3,
-      city: "Turku",
-      country: "Finland",
-    },
-    {
-      id: 4,
-      city: "Vaasa",
-      country: "Finland",
-    },
-    {
-      id: 5,
-      city: "Oulu",
-      country: "Finland",
-    },
+    { id: 1, city: "Helsinki", country: "Finland" },
+    { id: 2, city: "Turku", country: "Finland" },
+    { id: 3, city: "Vaasa", country: "Finland" },
+    { id: 4, city: "Oulu", country: "Finland" },
   ];
+
   return (
     <div className="nav-container">
       <nav>
@@ -38,13 +68,16 @@ export const Navbar = () => {
           <div className="input-group">
             <label htmlFor="location-input"></label>
             <input
+              type="search"
               list="locations"
               id="location-input"
               placeholder="Add location"
+              name="city"
+              onChange={handleChange}
             />
             <datalist id="locations">
               {navOption.map((opt) => (
-                <option key={opt.id} value={`${opt.city} ${opt.country}`} />
+                <option key={opt.id} value={opt.city} />
               ))}
             </datalist>
           </div>
@@ -55,9 +88,15 @@ export const Navbar = () => {
               id="guests-input"
               placeholder="Add guests"
               min="0"
+              name="maxGuests"
+              onChange={handleChange}
             />
           </div>
-          <button className="btn-search">
+          <button
+            value={searchValues.city}
+            onClick={filterData}
+            className="btn-search"
+          >
             <img src="/search.svg" alt="" />
           </button>
         </div>
